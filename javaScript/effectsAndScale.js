c33867c60ad6b40416ab63cf4a3d72322e575193
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-04-28 11:02:04 +0300

    add dra'n'drop and work with filters

--- javaScript/effectsAndScale.js
+++ javaScript/effectsAndScale.js
@@ -48,12 +48,18 @@
     //работа с фильтром фотографий
     var effectControls = document.querySelector(".upload-form");
     var effectInputs = effectControls.querySelectorAll("input[name=effect]");
-    
+    var uploadPin = document.querySelector(".upload-effect-level-pin");
+    var uploadLine = document.querySelector(".upload-effect-level-line");
+    var uploadlevel = document.querySelector(".upload-effect-level-val");
+
     var inputClickHendler = function(clickedEl,index){
         effectInputs[index].addEventListener("click", function(evt){
             var filterValue = effectInputs[index].getAttribute("value");
             var effectValueNow = workingFoto.classList[1];
             workingFoto.classList.remove(effectValueNow);
+            workingFoto.removeAttribute("style");
+            uploadPin.style.left = uploadLine.offsetWidth + "px";
+            uploadlevel.style.width = uploadLine.offsetWidth + "px";
             switch(filterValue){
                 case "chrome":
                     workingFoto.classList.add("effect-chrome");
@@ -76,4 +82,60 @@
     for (var i = 0; i < effectInputs.length; i++){
         inputClickHendler(effectInputs[i],i);
     }
+    //=================================
+    //drag
+    
+    uploadPin.addEventListener("mousedown",function(evt){
+        var startCoords = {
+            x: evt.clientX,
+        };
+        var onMouseMove = function(moveEvt){
+            moveEvt.preventDefault();
+
+            var shift = {
+                x: startCoords.x - moveEvt.clientX,
+            }
+
+            startCoords = {
+                x: moveEvt.clientX,
+            }
+            uploadPin.style.left = (uploadPin.offsetLeft - shift.x) + "px";
+            var dragedPinCoords = uploadPin.offsetLeft - shift.x;
+            var uploadLineWidth = uploadLine.offsetWidth;
+            uploadlevel.style.width = dragedPinCoords + "px";
+            if(dragedPinCoords <=  0){
+                uploadPin.style.left = 0 + "px"; 
+            }else if(dragedPinCoords >= uploadLineWidth){
+                uploadPin.style.left = uploadLineWidth + "px"; 
+            }
+            //приведение положения ползунка к процентам
+            procent = Math.floor(dragedPinCoords / (uploadLineWidth / 100));
+            //поиск фильтра который в данный момент на фото
+            var chrome = workingFoto.classList.contains("effect-chrome");
+            var sepia = workingFoto.classList.contains("effect-sepia");
+            var marvin = workingFoto.classList.contains("effect-marvin");
+            var phobos = workingFoto.classList.contains("effect-phobos");
+            var heat = workingFoto.classList.contains("effect-heat");
+            //присвоение атрибута фильтра
+            if(chrome){
+                workingFoto.style.filter = "grayscale" + "(" + procent/100 + ")";
+            }else if(sepia){
+                workingFoto.style.filter = "sepia" + "(" + procent/100 + ")";
+            }else if(marvin){
+                workingFoto.style.filter = "invert" + "(" + procent + "%)";
+            }else if(phobos){
+                workingFoto.style.filter = "blur" + "(" + Math.floor(procent/33) + "px)";
+            }else if(heat){
+                workingFoto.style.filter = "brightness" + "(" + Math.floor(procent/33) + ")";
+            }
+            
+        };
+        var onMouseUp = function(upEvt){
+            upEvt.preventDefault();
+            document.removeEventListener("mousemove", onMouseMove);
+            document.removeEventListener("mouseup", onMouseUp);
+        }
+        document.addEventListener("mousemove", onMouseMove);
+        document.addEventListener("mouseup", onMouseUp);
+    });
     })();
